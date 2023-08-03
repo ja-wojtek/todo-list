132be00ff27abfa22eced7e269382e79c3a23f22
Wojtek Mazurek <mazurek.wojciech88@gmail.com>
2023-08-03 07:49:04 +0200

    Set up script file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log(`It's nice to say hello.`)
+
+let todoInput;  // Wpisywanie
+let errorInfo;  // Infirmacja o braku zadań
+let addBtn;     // Przycisk Add
+let uList;      // Lista zadań, tagi UL
+
+const main = () => {
+  prepareDOMEelements();
+  prepareDOMEvents();
+}
+
+const prepareDOMEvenets = () => {
+
+}
+
+document.addEventListener('DOMContentLoaded', main);
