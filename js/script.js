fd11c8324bc2a5b2b2755a7bc78f3f9d475c2160
Wojtek Mazurek <mazurek.wojciech88@gmail.com>
2023-08-03 17:38:59 +0200

    Adding element

--- js/script.js
+++ js/script.js
@@ -1,17 +1,58 @@
-console.log(`It's nice to say hello.`)
-
-let todoInput;  // Wpisywanie
-let errorInfo;  // Infirmacja o braku zadań
-let addBtn;     // Przycisk Add
-let uList;      // Lista zadań, tagi UL
+let todoInput;   // Wpisywanie
+let errorInfo;   // Infirmacja o braku zadań
+let addBtn;      // Przycisk Add
+let uList;       // Lista zadań, tagi UL
+let newTodo;     // nowo dodane zadanie
 
 const main = () => {
   prepareDOMEelements();
   prepareDOMEvents();
 }
 
-const prepareDOMEvenets = () => {
+const prepareDOMEelements = () => {
+  todoInput = document.querySelector('.todo-input');
+  errorInfo = document.querySelector('.error-info');
+  addBtn = document.querySelector('.btn-add');
+  uList = document.querySelector('.todolist ul');
+}
 
+const prepareDOMEvents = () => {
+  addBtn.addEventListener('click', addNewTodo)
+}
+
+const addNewTodo = () => {
+  if (todoInput.value !== '') {
+    newTodo = document.createElement('li');
+    newTodo.textContent = todoInput.value;
+
+    createToolsArea();
+    uList.append(newTodo);
+
+    errorInfo.textContent = '';
+    todoInput.value = '';
+  } else {
+    errorInfo.textContent = 'Wpisz treść zadania';
+  }
+}
+
+const createToolsArea = () => {
+  const toolsPanel = document.createElement('div');
+  toolsPanel.classList.add('tools');
+  newTodo.append(toolsPanel);
+
+  const completeBtn = document.createElement('button');
+  completeBtn.classList.add('complete')
+  completeBtn.textContent = 'V'
+
+  const editBtn = document.createElement('button');
+  editBtn.classList.add('edit')
+  editBtn.textContent = 'EDIT'
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('delete')
+  deleteBtn.textContent = 'X'
+
+  toolsPanel.append(completeBtn, editBtn, deleteBtn);
 }
 
 document.addEventListener('DOMContentLoaded', main);
